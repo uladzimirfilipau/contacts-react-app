b51f864e2c211babd40170fa282ccd33cbb4c9da
Vova <uladzimirfilipau@gmail.com>
2023-02-20 14:47:12 +0300

    feat: root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Hello world!</h1>,
+    element: <Root />,
   },
 ]);
 
