247a59eee06e2c3aa8c1ae12f425b4eeebf02348
Vova <uladzimirfilipau@gmail.com>
2023-02-23 13:07:41 +0300

    feat: contactLoader

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
