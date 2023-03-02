b1d5c5d44ac5826518676bb0f702803705aeeb4f
Vova <uladzimirfilipau@gmail.com>
2023-03-02 11:51:38 +0300

    feat: contactAction

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,27 +1,64 @@
-import { Form, useLoaderData } from 'react-router-dom';
-import { getContact } from '../contacts';
+import { Form, useLoaderData, useFetcher } from 'react-router-dom';
+import { getContact, updateContact } from '../contacts';
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true',
+  });
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
-  return { contact };
+
+  if (!contact) {
+    throw new Response('', {
+      status: 404,
+      statusText: 'Not Found',
+    });
+  }
+
+  return contact;
+}
+
+function Favorite({ contact }) {
+  const fetcher = useFetcher();
+  let favorite = contact.favorite;
+
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true';
+  }
+
+  return (
+    <fetcher.Form method='post'>
+      <button
+        name='favorite'
+        value={favorite ? 'false' : 'true'}
+        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
+      >
+        {favorite ? '★' : '☆'}
+      </button>
+    </fetcher.Form>
+  );
 }
 
 export default function Contact() {
-  const { contact } = useLoaderData();
+  const contact = useLoaderData();
+  const { avatar, first, last, twitter, notes } = contact;
 
   return (
     <section id='contact'>
       {contact.avatar ? (
-        <img key={contact.avatar} src={contact.avatar} alt={contact.last} />
+        <img key={avatar} src={avatar} alt={last} />
       ) : (
         <img src={null} alt='No Avatar' />
       )}
 
       <div>
         <h1>
-          {contact.first || contact.last ? (
+          {first || last ? (
             <>
-              {contact.first} {contact.last}
+              {first} {last}
             </>
           ) : (
             <i>No Name</i>
@@ -29,15 +66,15 @@ export default function Contact() {
           <Favorite contact={contact} />
         </h1>
 
-        {contact.twitter && (
+        {twitter && (
           <p>
-            <a target='_blank' href={`https://twitter.com/${contact.twitter}`} rel='noreferrer'>
-              {contact.twitter}
+            <a target='_blank' href={`https://twitter.com/${twitter}`} rel='noreferrer'>
+              {twitter}
             </a>
           </p>
         )}
 
-        {contact.notes && <p>{contact.notes}</p>}
+        {notes && <p>{notes}</p>}
 
         <div>
           <Form action='edit'>
@@ -58,22 +95,5 @@ export default function Contact() {
         </div>
       </div>
     </section>
-  );
-}
-
-function Favorite({ contact }) {
-  let favorite = contact.favorite;
-
-  return (
-    <Form method='post'>
-      <button
-        type='button'
-        name='favorite'
-        value={favorite ? 'false' : 'true'}
-        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
-      >
-        {favorite ? '★' : '☆'}
-      </button>
-    </Form>
   );
 }
